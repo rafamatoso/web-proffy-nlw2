41770f5b0682bedb7d8ca43cbfcf6bccd3324492
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-12 21:13:49 -0300

    Algumas atualizações nas variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? `${process.env.REACT_APP_API_DEV}`
+      : `${process.env.REACT_APP_API_URL}`,
 });
 
 export default api;
